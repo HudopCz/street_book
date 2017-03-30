7420456e68d91a48eab0940d6b8debefd9d60202
Karel Jakubec <karel@apiwatcher.com>
2017-03-30 21:56:44 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add protipol data

--- street_book/startup.py
+++ street_book/startup.py
@@ -4,9 +4,11 @@ from street_book.app import app, api
 from street_book.config import Config
 
 from street_book.resources.book_page import BookPage
+from street_book.resources.book_page_code import BookPageCode
 from street_book.resources.codes import Codes
 
 api.add_resource(BookPage, "/books/<string:book_name>/pages/<string:page_id>")
+api.add_resource(BookPageCode, "/books/<string:book_name>/pages/<string:page_id>/qr")
 api.add_resource(Codes, "/books/<string:book_name>/qr")
 
 app.config.from_object(Config)
