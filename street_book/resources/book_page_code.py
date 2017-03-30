53abc3c812c73f2f00e2e75b5713b6daad35e718
Karel Jakubec <karel@apiwatcher.com>
2017-03-30 21:57:32 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add protipol data

---
+++ street_book/resources/book_page_code.py
@@ -0,0 +1,61 @@
+import jinja2
+import json
+import os
+import base64
+import qrcode
+import cStringIO
+
+from flask_restful import Resource
+from flask import make_response
+
+from street_book.app import app
+from street_book.books import books
+
+class BookPageCode(Resource):
+    """
+    Return content of one page if correct id is provided
+
+    This is the main resource of the application.
+    """
+
+    def get(self, book_name, page_id):
+        """
+        Return content for specific page of the book
+        """
+        this_dir = os.path.dirname(__file__)
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(
+                os.path.join(this_dir, '../templates')
+            )
+        )
+
+        if not books.get(book_name):
+            tmpl = env.get_template("404.html")
+            return make_response(tmpl.render({}), 404)
+        else:
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            url = '{0}/books/{1}/pages/{2}'.format(
+                app.config["BASE_URL"], book_name, page_id
+            )
+            qr.add_data(url)
+            qr.make(fit=True)
+            img = qr.make_image()
+
+            buffer = cStringIO.StringIO()
+            img.save(buffer, format="PNG")
+
+            tmpl = env.get_template("qr_code.html")
+            return make_response(tmpl.render(
+                {
+                    "name": page_id,
+                    "url": url,
+                    "img": "data:image/png;base64, " + base64.b64encode(
+                        buffer.getvalue()
+                    )
+                }
+            ))
